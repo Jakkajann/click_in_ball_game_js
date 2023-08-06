c633e524bd81bbbb5f0f39c3dda31c70f6515b19
fernando <fernandobismaia@gmail.com>
2023-08-06 13:34:36 -0300

    Start time function

--- script/start.js
+++ script/start.js
@@ -1,28 +1,40 @@
 const maxWidth = window.innerWidth - 150;
 const maxHeight = window.innerHeight - 150;
 let scoreValue = 0;
+let lives = 5;
 
 
-function getPos() {
+function createBall() {
     
-    const posX = Math.round(Math.random() * maxWidth);
-    const posY = Math.round(Math.random() * maxHeight);
+    const posX = String(Math.round(Math.random() * maxWidth));
+    const posY = String(Math.round(Math.random() * maxHeight));
+    const sizeFactor = (Math.random() * 5) + 1.01;
+    const startWidth = 20;
+    const startHeight = 20;
+    let clicked = false;
+
+    const score = document.querySelector("#scoreValue");
 
     const ball = document.createElement("div");
     ball.classList.add("ball");
     ball.style.position = "absolute";
-    ball.style.top = String(posY) + "px";
-    ball.style.left = String(posX) + "px";
+    ball.style.top = posY + "px";
+    ball.style.left = posX + "px";
+    ball.style.width = String(startWidth * sizeFactor) + "px";
+    ball.style.height = String(startHeight * sizeFactor) + "px";
 
-    ball.addEventListener("onclick", () => {
+    ball.addEventListener("click", () => {
+        scoreValue++;
+        score.innerHTML = scoreValue;
+        ball.remove();
+        clicked = true;
+    });
 
-    })
-    return ball;
+    document.querySelector("body").appendChild(ball);
 }
 
-
-document.body.appendChild(getPos());
-
-const score = document.querySelector("#scoreValue");
-
-console.log(score.innerHTML);
+let time = 3000;
+while (lives > 0) {
+    setTimeout(createBall, time);
+    time -= 100;
+}
